cd69aa9664554b62d4da5bcebd726c50d8606a2e
Bomber99-debug <andreyrybchenko16@gmail.com>
2026-02-22 19:53:54 +0200

    appdate

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Record:
             self.phones.remove(value)
 
     def edit_phone(self, value: str, replace: str) -> None:
-        """Замінює існуючий телефон на новий."""
+        """Замінює що існує телефон на новий."""
         phone_obj = self.find_phone(value)
         if phone_obj:
             self.remove_phone(phone_obj)
